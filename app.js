bfad0be788961f37a915b137f08c4cad7a4dbc73
Pedro H. Mendes <pedro057hm@gmail.com>
2025-03-13 01:56:33 -0300

    array de amigos e funcao de add amigos

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo() {
+
+    amigoInput = document.querySelector('input').value;
+    
+    if(amigoInput != ''){
+        amigos.push(amigoInput);
+        console.log(amigos);
+    } else {
+        alert("Preencha o campo com o nome do seu amigo");
+    }
+
+    limparCampo();
+}
+
+function limparCampo() {
+    campo = document.querySelector('input');
+    campo.value = '';
+}
